58c5abf5b659ffd40dfbcb240f850a867f49aaa3
Munazza Fatma <fatmamunazza@gmail.com>
2020-06-07 19:29:12 +0530

    Wallet Section Completed

--- app/src/main/java/com/example/serviceapp/Recharge.java
+++ app/src/main/java/com/example/serviceapp/Recharge.java
@@ -12,20 +12,32 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.razorpay.Checkout;
 import com.razorpay.PaymentData;
 import com.razorpay.PaymentResultListener;
 import com.razorpay.PaymentResultWithDataListener;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class Recharge extends AppCompatActivity implements PaymentResultListener {
 
    private EditText amount;
-    ImageView back;
-    TextView submit;
+    TextView submit,back;
+    private String payment;
 
     //KEY ID=rzp_test_V62hfKmlIk6uuQ;
     //KEY SECRET=I0s19VfEtEvkKq89DyqChap4;
@@ -45,7 +57,7 @@ public class Recharge extends AppCompatActivity implements PaymentResultListener
         amount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                amount.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_money,0,0,0);
+                amount.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_rupee,0,0,0);
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     amount.setBackground(getDrawable(R.drawable.edittext_select));
                 }
@@ -85,11 +97,12 @@ public class Recharge extends AppCompatActivity implements PaymentResultListener
             options.put("name", "Service App");
             options.put("description", "Recharge your wallet");
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //add url here for icon
                 options.put("image", "https://techfatimawebd.000webhostapp.com/img/icon.png");
             }
             options.put("currency", "INR");
 
-            String payment = amount.getText().toString();
+            payment = amount.getText().toString();
 
             double total = Double.parseDouble(payment);
             total = total * 100;
@@ -109,8 +122,46 @@ public class Recharge extends AppCompatActivity implements PaymentResultListener
     }
 
     @Override
-    public void onPaymentSuccess(String razorpayPaymentID) {
-        Toast.makeText(this, "Payment successfully done! " + razorpayPaymentID, Toast.LENGTH_SHORT).show();
+    public void onPaymentSuccess(final String razorpayPaymentID) {
+
+        JSONObject jsonObject=new JSONObject();
+        try {
+            jsonObject.put("amount",amount.getText().toString());
+            jsonObject.put("transaction_id",razorpayPaymentID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String url="http://serveondoor.com/servernew/Restapi/walletrecharge";
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url,jsonObject
+                , new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response", response.toString());
+                Toast.makeText(Recharge.this, "Payment successfully done! ", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error", "Error: " + error.getMessage());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            @Override
+            public HashMap<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("token","oUtsKvCtGd");
+                headers.put("Authorization", "token");
+                return headers;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(req);
+
 
     }
 
--- app/src/main/java/com/example/serviceapp/fragment/Wallet.java
+++ app/src/main/java/com/example/serviceapp/fragment/Wallet.java
@@ -5,15 +5,32 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.serviceapp.HistoryActivity;
 import com.example.serviceapp.R;
 import com.example.serviceapp.Recharge;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class Wallet extends Fragment {
 
@@ -21,7 +38,7 @@ public class Wallet extends Fragment {
         // Required empty public constructor
     }
 
-    TextView recharge;
+    TextView recharge,balance;
     RelativeLayout walletHistory;
 
     //KEY ID=rzp_test_V62hfKmlIk6uuQ;
@@ -34,7 +51,8 @@ public class Wallet extends Fragment {
         View view=inflater.inflate(R.layout.fragment_wallet, container, false);
 
         recharge=view.findViewById(R.id.recharge);
-        walletHistory=view.findViewById(R.id.wallet_history);
+        balance=view.findViewById(R.id.balance);
+      //  walletHistory=view.findViewById(R.id.wallet_history);
 
         recharge.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,13 +62,56 @@ public class Wallet extends Fragment {
             }
         });
 
-        walletHistory.setOnClickListener(new View.OnClickListener() {
+
+
+       /* walletHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), HistoryActivity.class));
                 getActivity().overridePendingTransition(R.anim.slide_right_in,R.anim.no_animation);
             }
-        });
+        });*/
+
+
+        String url="http://serveondoor.com/servernew/Restapi/walletdetails";
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.POST, url,null
+                , new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    JSONObject jsonObject=response.getJSONObject(0);
+                    String balanceValue=jsonObject.getString("ny_money");
+                     balance.setText(balanceValue);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.d("Response", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), String.valueOf(error), Toast.LENGTH_SHORT).show();
+
+                VolleyLog.d("Error", "Error: " + error.getMessage());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            @Override
+            public HashMap<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("token","oUtsKvCtGd");
+                headers.put("Authorization", "token");
+                return headers;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(req);
 
         return view;
     }
